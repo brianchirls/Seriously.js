ba4bdcd34f0ebe87dac8a7bdd15e034eec568853
Brian Chirls <brian@chirls.com>
2012-02-08 10:05:29 -0500

    Added 2d simplex noise to utils shader functions

--- seriously.js
+++ seriously.js
@@ -3209,6 +3209,53 @@ Seriously.util = {
 	}
 };
 
+/*
+ * simplex noise shaders
+ * https://github.com/ashima/webgl-noise
+ * Copyright (C) 2011 by Ashima Arts (Simplex noise)
+ * Copyright (C) 2011 by Stefan Gustavson (Classic noise)
+ */
+Seriously.util.shader.snoise2d = 'vec3 mod289_3(vec3 x) {\n' +
+	'	return x - floor(x * (1.0 / 289.0)) * 289.0;\n' +
+	'}\n' +
+	'vec2 mod289_2(vec2 x) {\n' +
+	'	return x - floor(x * (1.0 / 289.0)) * 289.0;\n' +
+	'}\n' +
+	'vec3 permute(vec3 x) {\n' +
+	'	return mod289_3(((x*34.0)+1.0)*x);\n' +
+	'}\n' +
+	'float snoise(vec2 v) {\n' +
+	'	const vec4 C = vec4(0.211324865405187, // (3.0-sqrt(3.0))/6.0\n' +
+	'		0.366025403784439, // 0.5*(sqrt(3.0)-1.0)\n' +
+	'	   -0.577350269189626, // -1.0 + 2.0 * C.x\n' +
+	'		0.024390243902439); // 1.0 / 41.0\n' +
+	'	vec2 i = floor(v + dot(v, C.yy) );\n' +
+	'	vec2 x0 = v - i + dot(i, C.xx);\n' +
+	'	vec2 i1;\n' +
+	'	//i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0\n' +
+	'	//i1.y = 1.0 - i1.x;\n' +
+	'	i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);\n' +
+	'	// x0 = x0 - 0.0 + 0.0 * C.xx ;\n' +
+	'	// x1 = x0 - i1 + 1.0 * C.xx ;\n' +
+	'	// x2 = x0 - 1.0 + 2.0 * C.xx ;\n' +
+	'	vec4 x12 = x0.xyxy + C.xxzz;\n' +
+	'	x12.xy -= i1;\n' +
+	'	i = mod289_2(i); // Avoid truncation effects in permutation\n' +
+	'	vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));\n' +
+	'	vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);\n' +
+	'	m = m*m ;\n' +
+	'	m = m*m ;\n' +
+	'	vec3 x = 2.0 * fract(p * C.www) - 1.0;\n' +
+	'	vec3 h = abs(x) - 0.5;\n' +
+	'	vec3 ox = floor(x + 0.5);\n' +
+	'	vec3 a0 = x - ox;\n' +
+	'	m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );\n' +
+	'	vec3 g;\n' +
+	'	g.x = a0.x * x0.x + h.x * x0.y;\n' +
+	'	g.yz = a0.yz * x12.xz + h.yz * x12.yw;\n' +
+	'	return 130.0 * dot(m, g);\n' +
+	'}\n';
+
 window.Seriously = Seriously;
 
 }(window));
