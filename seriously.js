6fc3fdfbec5b83af01baac988130c78ffe57638d
Brian Chirls <brian@chirls.com>
2012-08-10 09:44:02 -0400

    Added 3d, 4d simplex noise shader functions, random function

--- seriously.js
+++ seriously.js
@@ -3205,7 +3205,13 @@ Seriously.util = {
 					'	x = mod(x, 13.0) * mod(x, 127.0);\n' +
 					'	float dx = mod(x, 0.01);\n' +
 					'	return clamp(0.1 + dx * 100.0, 0.0,1.0);\n' +
-					'}\n'
+					'}\n',
+		random: '#ifndef RANDOM\n' +
+			'#define RANDOM\n' +
+			'float random(vec2 n) {\n' +
+			'	return 0.5 + 0.5 * fract(sin(dot(n.xy, vec2(12.9898, 78.233)))* 43758.5453);\n' +
+			'}\n' +
+			'#endif\n'
 	}
 };
 
@@ -3215,15 +3221,34 @@ Seriously.util = {
  * Copyright (C) 2011 by Ashima Arts (Simplex noise)
  * Copyright (C) 2011 by Stefan Gustavson (Classic noise)
  */
-Seriously.util.shader.snoise2d = 'vec3 mod289_3(vec3 x) {\n' +
+
+Seriously.util.shader.noiseHelpers = '#ifndef NOISE_HELPERS\n' +
+	'#define NOISE_HELPERS\n' +
+	'vec2 mod289(vec2 x) {\n' +
 	'	return x - floor(x * (1.0 / 289.0)) * 289.0;\n' +
 	'}\n' +
-	'vec2 mod289_2(vec2 x) {\n' +
+	'vec3 mod289(vec3 x) {\n' +
+	'	return x - floor(x * (1.0 / 289.0)) * 289.0;\n' +
+	'}\n' +
+	'vec4 mod289(vec4 x) {\n' +
 	'	return x - floor(x * (1.0 / 289.0)) * 289.0;\n' +
 	'}\n' +
 	'vec3 permute(vec3 x) {\n' +
-	'	return mod289_3(((x*34.0)+1.0)*x);\n' +
+	'	return mod289(((x*34.0)+1.0)*x);\n' +
 	'}\n' +
+	'vec4 permute(vec4 x) {\n' +
+	'	return mod289(((x*34.0)+1.0)*x);\n' +
+	'}\n' +
+	'vec4 taylorInvSqrt(vec4 r) {\n' +
+	'	return 1.79284291400159 - 0.85373472095314 * r;\n' +
+	'}\n' +
+	'float taylorInvSqrt(float r) {\n' +
+	'	return 1.79284291400159 - 0.85373472095314 * r;\n' +
+	'}\n' +
+	'#endif\n';
+
+Seriously.util.shader.snoise2d = '#ifndef NOISE2D\n' +
+	'#define NOISE2D\n' +
 	'float snoise(vec2 v) {\n' +
 	'	const vec4 C = vec4(0.211324865405187, // (3.0-sqrt(3.0))/6.0\n' +
 	'		0.366025403784439, // 0.5*(sqrt(3.0)-1.0)\n' +
@@ -3240,7 +3265,7 @@ Seriously.util.shader.snoise2d = 'vec3 mod289_3(vec3 x) {\n' +
 	'	// x2 = x0 - 1.0 + 2.0 * C.xx ;\n' +
 	'	vec4 x12 = x0.xyxy + C.xxzz;\n' +
 	'	x12.xy -= i1;\n' +
-	'	i = mod289_2(i); // Avoid truncation effects in permutation\n' +
+	'	i = mod289(i); // Avoid truncation effects in permutation\n' +
 	'	vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));\n' +
 	'	vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);\n' +
 	'	m = m*m ;\n' +
@@ -3254,7 +3279,176 @@ Seriously.util.shader.snoise2d = 'vec3 mod289_3(vec3 x) {\n' +
 	'	g.x = a0.x * x0.x + h.x * x0.y;\n' +
 	'	g.yz = a0.yz * x12.xz + h.yz * x12.yw;\n' +
 	'	return 130.0 * dot(m, g);\n' +
-	'}\n';
+	'}\n' +
+	'#endif\n';
+
+Seriously.util.shader.snoise3d = '#ifndef NOISE3D\n' +
+	'#define NOISE3D\n' +
+	'float snoise(vec3 v) {\n' +
+	'	const vec2 C = vec2(1.0/6.0, 1.0/3.0) ;\n' +
+	'	const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);\n' +
+
+	// First corner
+	'	vec3 i = floor(v + dot(v, C.yyy) );\n' +
+	'	vec3 x0 = v - i + dot(i, C.xxx) ;\n' +
+
+	// Other corners
+	'	vec3 g = step(x0.yzx, x0.xyz);\n' +
+	'	vec3 l = 1.0 - g;\n' +
+	'	vec3 i1 = min( g.xyz, l.zxy );\n' +
+	'	vec3 i2 = max( g.xyz, l.zxy );\n' +
+
+	'	// x0 = x0 - 0.0 + 0.0 * C.xxx;\n' +
+	'	// x1 = x0 - i1 + 1.0 * C.xxx;\n' +
+	'	// x2 = x0 - i2 + 2.0 * C.xxx;\n' +
+	'	// x3 = x0 - 1.0 + 3.0 * C.xxx;\n' +
+	'	vec3 x1 = x0 - i1 + C.xxx;\n' +
+	'	vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y\n' +
+	'	vec3 x3 = x0 - D.yyy; // -1.0+3.0*C.x = -0.5 = -D.y\n' +
+
+	// Permutations
+	'	i = mod289(i);\n' +
+	'	vec4 p = permute( permute( permute(\n' +
+	'						 i.z + vec4(0.0, i1.z, i2.z, 1.0 ))\n' +
+	'					 	+ i.y + vec4(0.0, i1.y, i2.y, 1.0 ))\n' +
+	'					 	+ i.x + vec4(0.0, i1.x, i2.x, 1.0 ));\n' +
+
+	// Gradients: 7x7 points over a square, mapped onto an octahedron.
+	// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+	'	float n_ = 0.142857142857; // 1.0/7.0\n' +
+	'	vec3 ns = n_ * D.wyz - D.xzx;\n' +
+
+	'	vec4 j = p - 49.0 * floor(p * ns.z * ns.z); // mod(p,7*7)\n' +
+
+	'	vec4 x_ = floor(j * ns.z);\n' +
+	'	vec4 y_ = floor(j - 7.0 * x_ ); // mod(j,N)\n' +
+
+	'	vec4 x = x_ *ns.x + ns.yyyy;\n' +
+	'	vec4 y = y_ *ns.x + ns.yyyy;\n' +
+	'	vec4 h = 1.0 - abs(x) - abs(y);\n' +
+
+	'	vec4 b0 = vec4( x.xy, y.xy );\n' +
+	'	vec4 b1 = vec4( x.zw, y.zw );\n' +
+
+	'	//vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;\n' +
+	'	//vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;\n' +
+	'	vec4 s0 = floor(b0)*2.0 + 1.0;\n' +
+	'	vec4 s1 = floor(b1)*2.0 + 1.0;\n' +
+	'	vec4 sh = -step(h, vec4(0.0));\n' +
+
+	'	vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;\n' +
+	'	vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;\n' +
+
+	'	vec3 p0 = vec3(a0.xy,h.x);\n' +
+	'	vec3 p1 = vec3(a0.zw,h.y);\n' +
+	'	vec3 p2 = vec3(a1.xy,h.z);\n' +
+	'	vec3 p3 = vec3(a1.zw,h.w);\n' +
+
+	//Normalise gradients
+	'	vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));\n' +
+	'	p0 *= norm.x;\n' +
+	'	p1 *= norm.y;\n' +
+	'	p2 *= norm.z;\n' +
+	'	p3 *= norm.w;\n' +
+
+	// Mix final noise value
+	'	vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);\n' +
+	'	m = m * m;\n' +
+	'	return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),\n' +
+	'							                  dot(p2,x2), dot(p3,x3) ) );\n' +
+	'}\n' +
+	'#endif\n';
+
+Seriously.util.shader.snoise4d = '#ifndef NOISE4D\n' +
+	'#define NOISE4D\n' +
+	'vec4 grad4(float j, vec4 ip)\n' +
+	'	{\n' +
+	'	const vec4 ones = vec4(1.0, 1.0, 1.0, -1.0);\n' +
+	'	vec4 p,s;\n' +
+	'\n' +
+	'	p.xyz = floor( fract (vec3(j) * ip.xyz) * 7.0) * ip.z - 1.0;\n' +
+	'	p.w = 1.5 - dot(abs(p.xyz), ones.xyz);\n' +
+	'	s = vec4(lessThan(p, vec4(0.0)));\n' +
+	'	p.xyz = p.xyz + (s.xyz*2.0 - 1.0) * s.www;\n' +
+	'\n' +
+	'	return p;\n' +
+	'	}\n' +
+	'\n' +
+	// (sqrt(5) - 1)/4 = F4, used once below\n
+	'#define F4 0.309016994374947451\n' +
+	'\n' +
+	'float snoise(vec4 v)\n' +
+	'	{\n' +
+	'	const vec4 C = vec4(0.138196601125011, // (5 - sqrt(5))/20 G4\n' +
+	'						0.276393202250021, // 2 * G4\n' +
+	'						0.414589803375032, // 3 * G4\n' +
+	'						-0.447213595499958); // -1 + 4 * G4\n' +
+	'\n' +
+	// First corner
+	'	vec4 i = floor(v + dot(v, vec4(F4)) );\n' +
+	'	vec4 x0 = v - i + dot(i, C.xxxx);\n' +
+	'\n' +
+	// Other corners
+	'\n' +
+	// Rank sorting originally contributed by Bill Licea-Kane, AMD (formerly ATI)
+	'	vec4 i0;\n' +
+	'	vec3 isX = step( x0.yzw, x0.xxx );\n' +
+	'	vec3 isYZ = step( x0.zww, x0.yyz );\n' +
+	// i0.x = dot( isX, vec3( 1.0 ) );
+	'	i0.x = isX.x + isX.y + isX.z;\n' +
+	'	i0.yzw = 1.0 - isX;\n' +
+	// i0.y += dot( isYZ.xy, vec2( 1.0 ) );
+	'	i0.y += isYZ.x + isYZ.y;\n' +
+	'	i0.zw += 1.0 - isYZ.xy;\n' +
+	'	i0.z += isYZ.z;\n' +
+	'	i0.w += 1.0 - isYZ.z;\n' +
+	'\n' +
+		// i0 now contains the unique values 0,1,2,3 in each channel
+	'	vec4 i3 = clamp( i0, 0.0, 1.0 );\n' +
+	'	vec4 i2 = clamp( i0-1.0, 0.0, 1.0 );\n' +
+	'	vec4 i1 = clamp( i0-2.0, 0.0, 1.0 );\n' +
+	'\n' +
+	'	vec4 x1 = x0 - i1 + C.xxxx;\n' +
+	'	vec4 x2 = x0 - i2 + C.yyyy;\n' +
+	'	vec4 x3 = x0 - i3 + C.zzzz;\n' +
+	'	vec4 x4 = x0 + C.wwww;\n' +
+	'\n' +
+	// Permutations
+	'	i = mod289(i);\n' +
+	'	float j0 = permute( permute( permute( permute(i.w) + i.z) + i.y) + i.x);\n' +
+	'	vec4 j1 = permute( permute( permute( permute (\n' +
+	'						 i.w + vec4(i1.w, i2.w, i3.w, 1.0 ))\n' +
+	'					 + i.z + vec4(i1.z, i2.z, i3.z, 1.0 ))\n' +
+	'					 + i.y + vec4(i1.y, i2.y, i3.y, 1.0 ))\n' +
+	'					 + i.x + vec4(i1.x, i2.x, i3.x, 1.0 ));\n' +
+	'\n' +
+	// Gradients: 7x7x6 points over a cube, mapped onto a 4-cross polytope
+	// 7*7*6 = 294, which is close to the ring size 17*17 = 289.
+	'	vec4 ip = vec4(1.0/294.0, 1.0/49.0, 1.0/7.0, 0.0) ;\n' +
+	'\n' +
+	'	vec4 p0 = grad4(j0, ip);\n' +
+	'	vec4 p1 = grad4(j1.x, ip);\n' +
+	'	vec4 p2 = grad4(j1.y, ip);\n' +
+	'	vec4 p3 = grad4(j1.z, ip);\n' +
+	'	vec4 p4 = grad4(j1.w, ip);\n' +
+	'\n' +
+	// Normalise gradients
+	'	vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));\n' +
+	'	p0 *= norm.x;\n' +
+	'	p1 *= norm.y;\n' +
+	'	p2 *= norm.z;\n' +
+	'	p3 *= norm.w;\n' +
+	'	p4 *= taylorInvSqrt(dot(p4,p4));\n' +
+	'\n' +
+	// Mix contributions from the five corners
+	'	vec3 m0 = max(0.6 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);\n' +
+	'	vec2 m1 = max(0.6 - vec2(dot(x3,x3), dot(x4,x4) ), 0.0);\n' +
+	'	m0 = m0 * m0;\n' +
+	'	m1 = m1 * m1;\n' +
+	'	return 49.0 * ( dot(m0*m0, vec3( dot( p0, x0 ), dot( p1, x1 ), dot( p2, x2 )))\n' +
+	'							 + dot(m1*m1, vec2( dot( p3, x3 ), dot( p4, x4 ) ) ) ) ;\n' +
+	'}\n' +
+	'#endif\n';
 
 window.Seriously = Seriously;
 
