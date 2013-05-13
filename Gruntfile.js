5ac773c30a1e3f4a3a8fdb65e13bbf34993e289a
Brian Chirls <brian@chirls.com>
2013-05-13 01:54:17 -0400

    First pass at Gruntfile.js for building

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	var banner = [
+		'/**',
+		' * @license',
+		' * <%= pkg.name %> - v<%= pkg.version %>',
+		' * Copyright (c) 2012, <%= pkg.author %>',
+		' * <%= pkg.homepage %>',
+		' *',
+		' * Compiled: <%= grunt.template.today("yyyy-mm-dd") %>',
+		' *',
+		' * <%= pkg.name %> is licensed under the <%= pkg.license %> License.',
+		' * <%= pkg.licenseUrl %>',
+		' */',
+		''
+	].join('\n');
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		dirs: {
+			build: 'build',
+			examples: 'examples',
+			test: 'test'
+		},
+		files: {
+			build: '<%= dirs.build %>/seriously.dev.js',
+			buildMin: '<%= dirs.build %>/seriously.min.js'
+		},
+		requirejs: {
+			compile: {
+				options: {
+					baseUrl: '.',
+					name: 'seriously',
+					out: '<%= files.build %>',
+					preserveLicenseComments: false,
+					useStrict: true,
+					optimize: 'none' //uglify after
+				}
+			}
+		},
+		/*
+		concat: {
+			options: {
+				banner: banner
+			},
+			dist: {
+				src: 'seriously.js',
+				dest: '<%= files.build %>'
+			}
+		},
+		*/
+		uglify: {
+			options: {
+				banner: banner
+			},
+			build: {
+				src: '<%= files.build %>',
+				dest: '<%= files.buildMin %>'
+			}
+		},
+		connect: {
+			qunit: {
+				options: {
+					port: grunt.option('port-test') || 9002,
+					base: './'
+				}
+			},
+			test: {
+				options: {
+					port: grunt.option('port-test') || 9002,
+					base: './',
+					keepalive: true
+				}
+			}
+		},
+		qunit: {
+			all: {
+				options: {
+					urls: ['http://localhost:' + (grunt.option('port-test') || 9002) + '/test/index.html']
+				}
+			}
+		}
+	});
+
+	//grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+
+	// Default task(s).
+	grunt.registerTask('default', ['requirejs', 'uglify']);
+	grunt.registerTask('build', ['requirejs', 'uglify']);
+	grunt.registerTask('test', ['connect:qunit', 'qunit']);
+};
