2bcc5a5d1ad6b1f478855d22eac8bf575ae2e1e8
Brian Chirls <brian@chirls.com>
2013-12-09 12:48:22 -0500

    Added camera source with example

---
+++ sources/seriously.camera.js
@@ -0,0 +1,103 @@
+/* global define, require */
+(function (root, factory) {
+	'use strict';
+
+	if (typeof exports === 'object') {
+		// Node/CommonJS
+		factory(require('seriously'));
+	} else if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['seriously'], factory);
+	} else {
+		/*
+		todo: build out-of-order loading for sources and transforms or remove this
+		if (!root.Seriously) {
+			root.Seriously = { plugin: function (name, opt) { this[name] = opt; } };
+		}
+		*/
+		factory(root.Seriously);
+	}
+}(this, function (Seriously, undefined) {
+	'use strict';
+
+	var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+	Seriously.source('camera', function (source, options, force) {
+		var me = this,
+			video,
+			destroyed = false,
+			stream;
+
+		function cleanUp() {
+			if (video) {
+				video.pause();
+				video.src = '';
+				video.load();
+			}
+
+			if (stream && stream.stop) {
+				stream.stop();
+			}
+			stream = null;
+		}
+
+		function initialize() {
+			me.width = video.videoWidth;
+			me.height = video.videoHeight;
+			me.resize();
+			me.initialize();
+		}
+
+		if (force) {
+			if (!getUserMedia) {
+				throw 'Camera source type unavailable. Browser does not support getUserMedia';
+			}
+
+			video = document.createElement('video');
+
+			getUserMedia.call(navigator, {
+				video: true
+			}, function (s) {
+				stream = s;
+
+				if (destroyed) {
+					cleanUp();
+					return;
+				}
+
+				if (window.webkitURL) {
+					video.src = window.webkitURL.createObjectURL(stream);
+				} else {
+					video.src = stream;
+				}
+
+				if (video.readyState >= 1) {
+					initialize();
+				} else {
+					video.addEventListener('loadedmetadata', function () {
+						if (!destroyed) {
+							initialize();
+						}
+					}, false);
+				}
+
+				video.play();
+			}, function (evt) {
+				console.log('Unable to access video camera', evt);
+			});
+
+			return {
+				deferTexture: true,
+				source: video,
+				render: Object.getPrototypeOf(this).renderVideo,
+				destroy: function () {
+					destroyed = true;
+					cleanUp();
+				}
+			};
+		}
+	}, {
+		//todo: compatible
+		title: 'Camera'
+	});
+}));
