5bbf30f410af338f86a5f2f7a09bfbe9a4819509
Brian Chirls <brian@chirls.com>
2014-01-03 17:55:55 -0500

    Add black to pre-computed color names that chrome doesn't provide

--- seriously.js
+++ seriously.js
@@ -51,6 +51,7 @@
 	// http://www.w3.org/TR/css3-color/#svg-color
 	colorNames = {
 		transparent: [0, 0, 0, 0],
+		black: [0, 0, 0, 1],
 		red: [1, 0, 0, 1],
 		green: [0, 1, 0, 1],
 		blue: [0, 0, 1, 1],
