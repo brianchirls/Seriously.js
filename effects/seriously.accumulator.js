e228161ca1b9b2b19dc8b5a628f6c47b19a5708c
Brian Chirls <brian@chirls.com>
2014-04-07 12:33:56 -0400

    Refactor Accumulator shader macros to work with IE11 (#23, #49)

--- effects/seriously.accumulator.js
+++ effects/seriously.accumulator.js
@@ -18,39 +18,60 @@
 	'use strict';
 
 	/*
-	Blend mode shader by Romain Dura
+	Adapted from blend mode shader by Romain Dura
 	http://mouaif.wordpress.com/2009/01/05/photoshop-math-with-glsl-shaders/
 	*/
 
+	function vectorBlendFormula(formula, base, blend) {
+		function replace(channel) {
+			var r = {
+				base: (base || 'base') + '.' + channel,
+				blend: (blend || 'blend') + '.' + channel
+			};
+			return function (match) {
+				return r[match] || match;
+			};
+		}
+
+		return 'vec3(' +
+			formula.replace(/blend|base/g, replace('r')) + ', ' +
+			formula.replace(/blend|base/g, replace('g')) + ', ' +
+			formula.replace(/blend|base/g, replace('b')) +
+			')';
+	}
+
+	var blendModes = {
+		normal: 'blend',
+		lighten: 'max(blend, base)',
+		darken: 'min(blend, base)',
+		multiply: '(base * blend)',
+		average: '(base + blend / TWO)',
+		add: 'min(base + blend, ONE)',
+		subtract: 'max(base + blend - ONE, ZERO)',
+		difference: 'abs(base - blend)',
+		negation: '(ONE - abs(ONE - base - blend))',
+		exclusion: '(base + blend - TWO * base * blend)',
+		screen: '(ONE - ((ONE - base) * (ONE - blend)))',
+		lineardodge: 'min(base + blend, ONE)',
+		phoenix: '(min(base, blend) - max(base, blend) + ONE)',
+		linearburn: 'max(base + blend - ONE, ZERO)', //same as subtract?
+
+		overlay: vectorBlendFormula('base < 0.5 ? (2.0 * base * blend) : (1.0 - 2.0 * (1.0 - base) * (1.0 - blend))'),
+		softlight: vectorBlendFormula('blend < 0.5 ? (2.0 * base * blend + base * base * (1.0 - 2.0 * blend)) : (sqrt(base) * (2.0 * blend - 1.0) + 2.0 * base * (1.0 - blend))'),
+		hardlight: vectorBlendFormula('base < 0.5 ? (2.0 * base * blend) : (1.0 - 2.0 * (1.0 - base) * (1.0 - blend))', 'blend', 'base'),
+		colordodge: vectorBlendFormula('blend == 1.0 ? blend : min(base / (1.0 - blend), 1.0)'),
+		colorburn: vectorBlendFormula('blend == 0.0 ? blend : max((1.0 - ((1.0 - base) / blend)), 0.0)'),
+		//linearlight: vectorBlendFormula('blend < 0.5 ? max(base + 2.0 * blend - 1.0, 0.0) : min(base + (2.0 * (blend - 0.5)), 1.0)'),
+		linearlight: vectorBlendFormula('BlendLinearLightf(base, blend)'),
+		vividlight: vectorBlendFormula('BlendVividLightf(base, blend)'),
+		pinlight: vectorBlendFormula('BlendPinLightf(base, blend)'),
+		hardmix: vectorBlendFormula('BlendHardMixf(base, blend)'),
+		reflect: vectorBlendFormula('BlendReflectf(base, blend)'),
+		glow: vectorBlendFormula('BlendReflectf(blend, base)')
+	};
+
 	Seriously.plugin('accumulator', function () {
-		var modes = {
-			normal: 'BlendNormal',
-			lighten: 'BlendLighten',
-			darken: 'BlendDarken',
-			multiply: 'BlendMultiply',
-			average: 'BlendAverage',
-			add: 'BlendAdd',
-			subtract: 'BlendSubtract',
-			difference: 'BlendDifference',
-			negation: 'BlendNegation',
-			exclusion: 'BlendExclusion',
-			screen: 'BlendScreen',
-			overlay: 'BlendOverlay',
-			softlight: 'BlendSoftLight',
-			hardlight: 'BlendHardLight',
-			colordodge: 'BlendColorDodge',
-			colorburn: 'BlendColorBurn',
-			lineardodge: 'BlendLinearDodge',
-			linearburn: 'BlendLinearBurn',
-			linearlight: 'BlendLinearLight',
-			vividlight: 'BlendVividLight',
-			pinlight: 'BlendPinLight',
-			hardmix: 'BlendHardMix',
-			reflect: 'BlendReflect',
-			glow: 'BlendGlow',
-			phoenix: 'BlendPhoenix'
-		},
-		drawOpts = {
+		var drawOpts = {
 			clear: false
 		},
 		frameBuffers,
@@ -67,17 +88,19 @@
 			shader: function (inputs, shaderSource) {
 				var mode = inputs.blendMode || 'normal';
 				mode = mode.toLowerCase();
-				mode = modes[mode] || 'BlendNormal';
 
 				shaderSource.fragment = [
-					'#define BlendFunction ' + mode,
-
 					'precision mediump float;',
 
-					'#define BlendLinearDodgef				BlendAddf',
-					'#define BlendLinearBurnf				BlendSubtractf',
+					'const vec3 ZERO = vec3(0.0);',
+					'const vec3 ONE = vec3(1.0);',
+					'const vec3 HALF = vec3(0.5);',
+					'const vec3 TWO = vec3(2.0);',
+
 					'#define BlendAddf(base, blend)			min(base + blend, 1.0)',
 					'#define BlendSubtractf(base, blend)	max(base + blend - 1.0, 0.0)',
+					'#define BlendLinearDodgef(base, blend)	BlendAddf(base, blend)',
+					'#define BlendLinearBurnf(base, blend)	BlendSubtractf(base, blend)',
 					'#define BlendLightenf(base, blend)		max(blend, base)',
 					'#define BlendDarkenf(base, blend)		min(blend, base)',
 					'#define BlendLinearLightf(base, blend)	(blend < 0.5 ? BlendLinearBurnf(base, (2.0 * blend)) : BlendLinearDodgef(base, (2.0 * (blend - 0.5))))',
@@ -90,41 +113,15 @@
 					'#define BlendPinLightf(base, blend)	((blend < 0.5) ? BlendDarkenf(base, (2.0 * blend)) : BlendLightenf(base, (2.0 *(blend - 0.5))))',
 					'#define BlendHardMixf(base, blend)		((BlendVividLightf(base, blend) < 0.5) ? 0.0 : 1.0)',
 					'#define BlendReflectf(base, blend)		((blend == 1.0) ? blend : min(base * base / (1.0 - blend), 1.0))',
+
 					/*
-					** Vector3 blending modes
+					Linear Light is another contrast-increasing mode
+					If the blend color is darker than midgray, Linear Light darkens the image
+					by decreasing the brightness. If the blend color is lighter than midgray,
+					the result is a brighter image due to increased brightness.
 					*/
 
-					// Component wise blending
-					'#define Blend(base, blend, funcf)		vec3(funcf(base.r, blend.r), funcf(base.g, blend.g), funcf(base.b, blend.b))',
-					'#define BlendNormal(base, blend)		(blend)',
-					'#define BlendLighten					BlendLightenf',
-					'#define BlendDarken					BlendDarkenf',
-					'#define BlendMultiply(base, blend)		(base * blend)',
-					'#define BlendAverage(base, blend)		((base + blend) / 2.0)',
-					'#define BlendAdd(base, blend)			min(base + blend, vec3(1.0))',
-					'#define BlendSubtract(base, blend)	max(base + blend - vec3(1.0), vec3(0.0))',
-					'#define BlendDifference(base, blend)	abs(base - blend)',
-					'#define BlendNegation(base, blend)		(vec3(1.0) - abs(vec3(1.0) - base - blend))',
-					'#define BlendExclusion(base, blend)	(base + blend - 2.0 * base * blend)',
-					'#define BlendScreen(base, blend)		Blend(base, blend, BlendScreenf)',
-					'#define BlendOverlay(base, blend)		Blend(base, blend, BlendOverlayf)',
-					'#define BlendSoftLight(base, blend)	Blend(base, blend, BlendSoftLightf)',
-					'#define BlendHardLight(base, blend)	BlendOverlay(blend, base)',
-					'#define BlendColorDodge(base, blend)	Blend(base, blend, BlendColorDodgef)',
-					'#define BlendColorBurn(base, blend)	Blend(base, blend, BlendColorBurnf)',
-					'#define BlendLinearDodge				BlendAdd',
-					'#define BlendLinearBurn				BlendSubtract',
-					// Linear Light is another contrast-increasing mode
-					// If the blend color is darker than midgray, Linear Light darkens the image by decreasing the brightness. If the blend color is lighter than midgray, the result is a brighter image due to increased brightness.
-					'#define BlendLinearLight(base, blend)	Blend(base, blend, BlendLinearLightf)',
-					'#define BlendVividLight(base, blend)	Blend(base, blend, BlendVividLightf)',
-					'#define BlendPinLight(base, blend)		Blend(base, blend, BlendPinLightf)',
-					'#define BlendHardMix(base, blend)		Blend(base, blend, BlendHardMixf)',
-					'#define BlendReflect(base, blend)		Blend(base, blend, BlendReflectf)',
-					'#define BlendGlow(base, blend)			BlendReflect(blend, base)',
-					'#define BlendPhoenix(base, blend)		(min(base, blend) - max(base, blend) + vec3(1.0))',
-					//'#define BlendOpacity(base, blend, F, O)	(F(base, blend) * O + blend * (1.0 - O))',
-					//'#define BlendOpacity(base, blend, BlendFn, Opacity, Alpha)	((BlendFn(base.rgb * blend.a * Opacity, blend.rgb * blend.a * Opacity) + base.rgb * base.a * (1.0 - blend.a * Opacity)) / Alpha)',
+					'#define BlendFunction(base, blend) ' + blendModes[mode],
 
 					'varying vec2 vTexCoord;',
 					'varying vec4 vPosition;',
