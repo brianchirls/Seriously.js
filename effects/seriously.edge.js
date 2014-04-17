245c483c67bb12602bb12f845a406de9cbb2918a
Brian Chirls <brian@chirls.com>
2014-04-17 18:12:19 -0400

    Edge efect updates uniforms when the change, not on every draw

--- effects/seriously.edge.js
+++ effects/seriously.edge.js
@@ -60,6 +60,18 @@
 	]);
 
 	Seriously.plugin('edge', {
+		initialize: function (initialize) {
+			initialize();
+
+			this.uniforms.pixelWidth = 1 / this.width;
+			this.uniforms.pixelHeight = 1 / this.height;
+
+			if (this.inputs.mode === 'sobel') {
+				this.uniforms['G[0]'] = sobelMatrixConstants;
+			} else {
+				this.uniforms['G[0]'] = freiChenMatrixConstants;
+			}
+		},
 		shader: function (inputs, shaderSource) {
 			var defines;
 
@@ -127,18 +139,9 @@
 
 			return shaderSource;
 		},
-		draw: function (shader, model, uniforms, frameBuffer, parent) {
-
-			uniforms.pixelWidth = 1 / this.width;
-			uniforms.pixelHeight = 1 / this.height;
-
-			if (this.inputs.mode === 'sobel') {
-				uniforms['G[0]'] = sobelMatrixConstants;
-			} else {
-				uniforms['G[0]'] = freiChenMatrixConstants;
-			}
-
-			parent(shader, model, uniforms, frameBuffer);
+		resize: function () {
+			this.uniforms.pixelWidth = 1 / this.width;
+			this.uniforms.pixelHeight = 1 / this.height;
 		},
 		inputs: {
 			source: {
@@ -152,7 +155,14 @@
 				options: [
 					['sobel', 'Sobel'],
 					['frei-chen', 'Frei-Chen']
-				]
+				],
+				update: function () {
+					if (this.inputs.mode === 'sobel') {
+						this.uniforms['G[0]'] = sobelMatrixConstants;
+					} else {
+						this.uniforms['G[0]'] = freiChenMatrixConstants;
+					}
+				}
 			}
 		},
 		description: 'Edge Detect',
