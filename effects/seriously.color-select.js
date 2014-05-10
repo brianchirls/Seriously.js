170074a4f00004e791930d8e9ecdf0bcdadbb913
Brian Chirls <brian@chirls.com>
2014-05-09 20:47:35 -0400

    Vertex shader in Color Select needs to use standard position calculations

--- effects/seriously.color-select.js
+++ effects/seriously.color-select.js
@@ -22,9 +22,10 @@
 			shaderSource.vertex = [
 				'precision mediump float;',
 
-				'attribute vec3 position;',
+				'attribute vec4 position;',
 				'attribute vec2 texCoord;',
 
+				'uniform vec2 resolution;',
 				'uniform mat4 transform;',
 
 				'uniform float hueMin;',
@@ -46,8 +47,16 @@
 				'varying vec4 lightnessRange;',
 
 				'void main(void) {',
-				'	gl_Position = transform * vec4(position, 1.0);',
-				'	vTexCoord = vec2(texCoord.s, texCoord.t);',
+				// first convert to screen space
+				'	vec4 screenPosition = vec4(position.xy * resolution / 2.0, position.z, position.w);',
+				'	screenPosition = transform * screenPosition;',
+
+				// convert back to OpenGL coords
+				'	gl_Position.xy = screenPosition.xy * 2.0 / resolution;',
+				'	gl_Position.z = screenPosition.z * 2.0 / (resolution.x / resolution.y);',
+				'	gl_Position.w = screenPosition.w;',
+				'	vTexCoord = texCoord;',
+
 				'	float hueOffset = hueMin - hueMinFalloff;',
 				'	adjustedHueRange = mod(vec4(' +
 						'hueOffset, ' +
