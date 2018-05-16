a7a9ad2618dcb73f013406f453cf67a7e8ebd977
jbeckwith <josh.beckwith@toolofna.com>
2018-05-15 23:57:35 -0700

    enable float type

--- seriously.js
+++ seriously.js
@@ -779,7 +779,7 @@
 			status,
 			useFloat = options === true ? options : (options && options.useFloat);
 
-		useFloat = false;//useFloat && !!gl.getExtension('OES_texture_float'); //useFloat is not ready!
+		useFloat = useFloat && !!gl.getExtension('OES_texture_float');
 		if (useFloat) {
 			this.type = gl.FLOAT;
 		} else {
@@ -797,8 +797,8 @@
 			this.texture = gl.createTexture();
 			gl.bindTexture(gl.TEXTURE_2D, this.texture);
 			gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 			this.ownTexture = true;
@@ -878,8 +878,11 @@
 		gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer);
 		gl.bindRenderbuffer(gl.RENDERBUFFER, this.renderBuffer);
 
-		//todo: handle float
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+		if(this.type === gl.FLOAT){
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, new Float32Array(width * height * 4));
+		}else{
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+		}
 		gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height);
 		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.texture, 0);
 
